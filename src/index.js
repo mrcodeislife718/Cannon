cb04d98f309fb836bba0b20c77049c5cabb47ae7
Charles Castillo <mrcodeislife718@proton.me>
2026-08-11 12:20:39 -0400

    Export Cannon semantic analyzer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { lex, CannonSyntaxError } from './lexer.js';
 export { parse } from './parser.js';
+export { analyze, CannonSemanticError } from './semantic.js';
 export { compile, emitJavaScript } from './compiler.js';
