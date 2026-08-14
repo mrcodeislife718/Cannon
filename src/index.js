8b82a05fdcd278047f3bf48bc9fa833ad70db935
Charles Castillo <mrcodeislife718@proton.me>
2026-08-14 01:59:36 -0400

    Expose Cannon integrated toolchain

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { lex, CannonSyntaxError } from './lexer.js';
 export { parse } from './parser.js';
 export { compile, emitJavaScript } from './compiler.js';
+export { format, check, run, addDependency, install, discoverTests, runTests, createProject, moduleResolver } from './toolchain.js';
