f1e3776cda483c1c4d7d477c9b29872fe93bec6b
Charles Castillo <mrcodeislife718@proton.me>
2026-09-03 03:45:08 -0400

    Export recovered Cannon semantic analyzer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { lex, CannonSyntaxError } from './lexer.js';
 export { parse } from './parser.js';
+export { analyze, CannonSemanticError } from './semantic.js';
 export { compile, emitJavaScript } from './compiler.js';
 export { format, check, run, addDependency, install, discoverTests, runTests, createProject, moduleResolver } from './toolchain.js';
 export { buildTarget, executeTarget, reproducibleTargetBuild, benchmarkCompiler, emitNativeC } from './targets.js';
