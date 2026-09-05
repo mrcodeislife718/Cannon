92fb808a98921ebe611e0639e84c5584826ba0e5
Charles Castillo <mrcodeislife718@proton.me>
2026-09-05 14:12:17 -0400

    Export Cannon frontend artifact contract

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ export { lex, CannonSyntaxError } from './lexer.js';
 export { parse } from './parser.js';
 export { analyze, CannonSemanticError } from './semantic.js';
 export { compile, emitJavaScript } from './compiler.js';
+export { createFrontendArtifact, verifyFrontendArtifact, serializeFrontendArtifact, parseFrontendArtifact, CANNON_FRONTEND_PROTOCOL } from './frontend-contract.js';
 export { format, check, run, addDependency, install, discoverTests, runTests, createProject, moduleResolver } from './toolchain.js';
 export { buildTarget, executeTarget, reproducibleTargetBuild, benchmarkCompiler, emitNativeC } from './targets.js';
