10f3c8cb9ebac8fbf49be40595520011bb44c0d5
Charles Castillo <mrcodeislife718@proton.me>
2026-09-05 14:50:54 -0400

    Export Cannon production registry client

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ export { parse } from './parser.js';
 export { analyze, CannonSemanticError } from './semantic.js';
 export { compile, emitJavaScript } from './compiler.js';
 export { createFrontendArtifact, verifyFrontendArtifact, serializeFrontendArtifact, parseFrontendArtifact, CANNON_FRONTEND_PROTOCOL } from './frontend-contract.js';
-export { format, check, run, addDependency, install, discoverTests, runTests, createProject, moduleResolver } from './toolchain.js';
+export { format, check, run, HttpRegistryClient, registryFromEnvironment, addDependency, install, discoverTests, runTests, createProject, moduleResolver } from './toolchain.js';
 export { buildTarget, executeTarget, reproducibleTargetBuild, benchmarkCompiler, emitNativeC } from './targets.js';
