cef16244c997ee5ae39d81fc56a5a009e32f41e8
Charles Castillo <mrcodeislife718@proton.me>
2026-09-06 15:53:13 -0400

    Add Python-like for-in keyword to Cannon

--- src/lexer.js
+++ src/lexer.js
@@ -1,4 +1,4 @@
-const KEYWORDS = new Set(['fn','async','await','return','if','else','while','for','break','continue','true','false','null','let','const','import','export','from','as','default']);
+const KEYWORDS = new Set(['fn','async','await','return','if','else','while','for','in','break','continue','true','false','null','let','const','import','export','from','as','default']);
 
 export class CannonSyntaxError extends SyntaxError {
   constructor(message, line, column) {
