8f30791a002e8b7c32559d1b60a06902f5505196
Charles Castillo <mrcodeislife718@proton.me>
2026-09-06 12:52:34 -0400

    Expose Cannon module graph compiler

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export { lex, CannonSyntaxError } from './lexer.js';
 export { parse } from './parser.js';
 export { analyze, CannonSemanticError } from './semantic.js';
 export { compile, emitJavaScript } from './compiler.js';
+export { compileModuleGraph, writeModuleGraph, CannonModuleGraphError } from './module-graph.js';
 export { createFrontendArtifact, verifyFrontendArtifact, serializeFrontendArtifact, parseFrontendArtifact, CANNON_FRONTEND_PROTOCOL } from './frontend-contract.js';
 export { format } from './formatter.js';
 export { check, run, HttpRegistryClient, registryFromEnvironment, addDependency, install, discoverTests, runTests, createProject, moduleResolver } from './toolchain.js';
